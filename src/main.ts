5391f83ea50277ddf64c4f00e8ae04eb54d7003f
ledwastaken <ledwastaken@proton.me>
2025-10-24 15:42:58 +0200

    refactor(main): increase sphere's segments and rings

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Vector3 } from "./types/vector3";
 
 await engine.init();
 
-let ball = new Sphere();
+let ball = new Sphere(1, 64, 64);
 ball.cframe.add(new Vector3(0, 0, -2));
 
 engine.meshes.push(ball);
